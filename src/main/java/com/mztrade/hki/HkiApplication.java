014c585e9f26ffb76625b63d73063d7195bda590
hyungkya <36350256+hyungkya@users.noreply.github.com>
2024-02-19 15:44:00 +0900

    fix: 배포를 위해 모든 cors 허용

--- src/main/java/com/mztrade/hki/HkiApplication.java
+++ src/main/java/com/mztrade/hki/HkiApplication.java
@@ -7,6 +7,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 @SpringBootApplication
 public class HkiApplication {
     public static void main(String[] args) {
@@ -19,7 +24,7 @@ public class HkiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:62772")
+                        .allowedOrigins("*")
                         .allowedMethods(
                                 HttpMethod.GET.name(),
                                 HttpMethod.HEAD.name(),
