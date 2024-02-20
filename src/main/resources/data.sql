f3d53978c7f39bd996fa6b5ce48de1d92f03480b
hyungkya <36350256+hyungkya@users.noreply.github.com>
2024-02-20 22:40:55 +0900

    feat: 태그 초기값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -46358,3 +46358,10 @@ values  (1, '000270', 15, 52600.00),
 
 insert into hkidb.backtest_history (uid, aid, param, plratio)
 values  (1, 1, '{"dca": [0.25, 0.25, 0.25, 0.25], "uid": "1", "title": "포스트맨예시", "tickers": ["000270", "000660", "003670", "005380", "005490"], "end_date": "20151231", "start_date": "20120101", "max_trading": "3", "buy_conditions": [[{"frequency": "1, 1", "compare_type": ">", "base_indicator": "MACD_SIGNAL, 12, 26, 9", "constant_bound": "0", "target_indicator": "MACD, 12, 26"}]], "initial_balance": "10000000", "sell_conditions": [[{"frequency": "1, 1", "compare_type": "<", "base_indicator": "MACD_SIGNAL, 12, 26, 9", "constant_bound": "0", "target_indicator": "MACD, 12, 26"}]]}', 0.2553316000000001);
+
+INSERT INTO hkidb.tag (uid, tname, tcolor, category) VALUES (1, '뇌절매매', '0xFF005b96', 1);
+INSERT INTO hkidb.tag (uid, tname, tcolor, category) VALUES (1, '코스피', '0xFFf6abb6', 1);
+INSERT INTO hkidb.tag (uid, tname, tcolor, category) VALUES (1, '코스닥쳐', '0xFFf6cd61', 1);
+INSERT INTO hkidb.tag (uid, tname, tcolor, category) VALUES (1, '뇌절매매', '0xFF005b96', 2);
+INSERT INTO hkidb.tag (uid, tname, tcolor, category) VALUES (1, '코스피', '0xFFf6abb6', 2);
+INSERT INTO hkidb.tag (uid, tname, tcolor, category) VALUES (1, '코스닥쳐', '0xFFf6cd61', 2);
