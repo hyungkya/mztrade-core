daa8075b9aefa0d7f533ac976f3fe718404d44b5
youjunggen <yjg5858@naver.com>
2024-03-28 18:55:17 +0900

    feat: 백테스트 엔티티에 유저이름 추가

--- src/main/java/com/mztrade/hki/dto/BacktestHistoryResponse.java
+++ src/main/java/com/mztrade/hki/dto/BacktestHistoryResponse.java
@@ -12,12 +12,14 @@ import java.time.LocalDateTime;
 @Builder(toBuilder = true)
 public class BacktestHistoryResponse {
     private Integer uid;
+    private String uname;
     private Integer aid;
     private String param;
     private Double plratio;
     static public BacktestHistoryResponse from(BacktestHistory backtestHistory) {
         return BacktestHistoryResponse.builder()
                 .uid(backtestHistory.getUser().getUid())
+                .uname(backtestHistory.getUser().getName())
                 .aid(backtestHistory.getAccount().getAid())
                 .param(backtestHistory.getParam())
                 .plratio(backtestHistory.getPlratio())
