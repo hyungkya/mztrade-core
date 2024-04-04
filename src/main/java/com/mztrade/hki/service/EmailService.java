bf979ccd48ffd207ec37381f576d23761f235fb8
youjunggen <yjg5858@naver.com>
2024-04-04 19:17:14 +0900

    feat: smtp 메일 제목 수정

--- src/main/java/com/mztrade/hki/service/EmailService.java
+++ src/main/java/com/mztrade/hki/service/EmailService.java
@@ -45,7 +45,7 @@ public class EmailService {
         makeRandomNumber();
         String setFrom = "mztrade.dev@gmail.com"; // email-config에 설정한 자신의 이메일 주소를 입력
         String toMail = email;
-        String title = "[MZ Trading] 회원 가입 인증 번호"; // 이메일 제목
+        String title = "[MZ Trading] 인증 번호"; // 이메일 제목
         String content =
                 "MZ Trading을 방문해주셔서 감사합니다." +    //html 형식으로 작성 !
                         "<br><br>" +
