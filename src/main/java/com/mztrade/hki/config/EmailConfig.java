72af864b0203121e0493d6c62156779bd00214d6
hyungkya <36350256+hyungkya@users.noreply.github.com>
2024-04-05 16:16:13 +0900

    feat: SMTP SSL 인증 서버 주소 변경

--- src/main/java/com/mztrade/hki/config/EmailConfig.java
+++ src/main/java/com/mztrade/hki/config/EmailConfig.java
@@ -25,7 +25,7 @@ public class EmailConfig {
         javaMailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");//SSL 소켓 팩토리 클래스 사용
         javaMailProperties.put("mail.smtp.starttls.enable", "true");//STARTTLS(TLS를 시작하는 명령)를 사용하여 암호화된 통신을 활성화
         javaMailProperties.put("mail.debug", "true");//디버깅 정보 출력
-        javaMailProperties.put("mail.smtp.ssl.trust", "smtp.naver.com");//smtp 서버의 ssl 인증서를 신뢰
+        javaMailProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");//smtp 서버의 ssl 인증서를 신뢰
         javaMailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2");//사용할 ssl 프로토콜 버젼
 
         mailSender.setJavaMailProperties(javaMailProperties);//mailSender에 우리가 만든 properties 넣고
