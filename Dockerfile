46a64f23ac92b37438ab22c004d81e3e3ac89cb4
hyungkya <36350256+hyungkya@users.noreply.github.com>
2024-05-03 01:16:15 +0900

    feat: CI/CD를 위한 Github Actions 스크립트 도입

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM bellsoft/liberica-openjdk-alpine:21
 VOLUME /tmp
-ARG JAR_FILE=build/libs/HKI-0.0.1-SNAPSHOT.jar
+ARG JAR_FILE=build/libs/*.jar
 COPY ${JAR_FILE} app.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/test/java/com/mztrade/hki/HkiApplicationTests.java
+++ src/test/java/com/mztrade/hki/HkiApplicationTests.java
@@ -1,15 +1,9 @@
 package com.mztrade.hki;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class HkiApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
-
-
 }
