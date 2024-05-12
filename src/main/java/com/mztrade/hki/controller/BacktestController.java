23e69e3b2939abea00c07886f171c6b4bba5d896
hyungkya <36350256+hyungkya@users.noreply.github.com>
2024-05-12 13:40:15 +0900

    refactor: RESTful하게 API 수정
    /stock_price/indicator(s) -> /stock/{ticker}/indicator

--- src/main/java/com/mztrade/hki/controller/BacktestController.java
+++ src/main/java/com/mztrade/hki/controller/BacktestController.java
@@ -170,15 +170,27 @@ public class BacktestController {
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
-    @GetMapping("/stock_price/indicator")
-    public ResponseEntity<Double> getSingleIndicatorByTicker(@RequestParam String ticker,
-            @RequestParam String date, @RequestParam String type, @RequestParam List<Float> param) {
-        log.info(String.format("[GET] /stock_price/indicator/ticker=%s&date=%s&type=%s&param=%s",
-                ticker, date, type, param));
+    @GetMapping("/stock/{ticker}/indicator")
+    public ResponseEntity<?> getSingleIndicatorByTicker(
+            @PathVariable String ticker,
+            @RequestParam String startDate,
+            @RequestParam(defaultValue = "") String endDate,
+            @RequestParam String type,
+            @RequestParam List<Float> param
+    ) {
+        log.info(String.format("[GET] /stock/%s/indicator?startDate=%s&endDate=%s&type=%s&param=%s",
+                ticker, startDate, endDate, type, param));
 
-        return new ResponseEntity<>(
-                indicatorService.getIndicator(ticker, Util.stringToLocalDateTime(date), type,
-                        param), HttpStatus.OK);
+        if (!endDate.isEmpty()) {
+            return new ResponseEntity<>(
+                    indicatorService.getIndicators(ticker, Util.stringToLocalDateTime(startDate),
+                            Util.stringToLocalDateTime(endDate),
+                            Indicator.builder().type(type).params(param).build()), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(
+                    indicatorService.getIndicator(ticker, Util.stringToLocalDateTime(startDate), type,
+                            param), HttpStatus.OK);
+        }
     }
 
     @GetMapping("/stock_price/indicators")
